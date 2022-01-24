a980eb98c9171f526eb5839fd4a26d43078d87c6
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-01-24 10:57:12 +0530

    Update MyProjects.js

--- src/components/myProjects/MyProjects.js
+++ src/components/myProjects/MyProjects.js
@@ -52,13 +52,13 @@ const MyProjects = () => {
         <div className={classes.div4}>
           <img src={cyf} alt="cyf project" className={classes.myProjects} />
           <div className={classes.textContainer}>
-            <h3>Online Video-Convertor</h3>
-            <p>A Web App to convert .mp4 to hls .m3u8 using react and NodeJs framework with FFMPEG plugin</p>
+            <h3>Recpie mealDb</h3>
+            <p>A Web App which provide recipes to cook various dishes built using React, Node.js</p>
           </div>
           <div className={classes.btnContainer}>
-            <a href="https://video-convertor-mp4-to-hls-m3u8.netlify.app/"  target="_blank" rel="noopener noreferrer"><button>See Preview</button></a>
-            <a href="https://github.com/ankesai8/Video-Convertor-FrontEnd"  target="_blank" rel="noopener noreferrer"><button>FrontEnd</button></a>
-              <a href="https://github.com/ankesai8/Video-Convertor-BackEnd"  target="_blank" rel="noopener noreferrer"><button>BackEnd</button></a>  
+            <a href="https://mealdb-9989.netlify.app/"  target="_blank" rel="noopener noreferrer"><button>See Preview</button></a>
+            <a href="https://github.com/ankesai8/theMealDB-Recipe-Front-End"  target="_blank" rel="noopener noreferrer"><button>FrontEnd</button></a>
+              <a href="https://github.com/ankesai8/theMealDB-Recipe-Back-End"  target="_blank" rel="noopener noreferrer"><button>BackEnd</button></a>  
           </div>
         </div>
 
