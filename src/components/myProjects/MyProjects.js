72d1fb88c5d090c81539a27eb00feafb0ea6a0b0
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-01-29 07:01:00 +0530

    Update MyProjects.js

--- src/components/myProjects/MyProjects.js
+++ src/components/myProjects/MyProjects.js
@@ -18,7 +18,7 @@ const MyProjects = () => {
             <p>Online-Book-Store is place where you can buy your favorite books. It is built using MERN stack and Redux with payment gateway integrated. It has all the major features like Admin Dashboard, Shopping Cart, Order etc.</p>
           </div>
           <div className={classes.btnContainer}>
-            <a href="https://book-store-9999.herokuapp.com/" target="_blank" rel="noopener noreferrer"><button className={classes.btnProj}>See Preview</button></a>
+            <a href="https://book-store-9999.herokuapp.com/" target="_blank" rel="noopener noreferrer"><button className={classes.btnProj}>Go Live</button></a>
             <a href="https://github.com/ankesai8/Online-Book-Store/tree/main/frontend" target="_blank" rel="noopener noreferrer"><button className={classes.btnProj}>FrontEnd</button></a>
             <a href="https://github.com/ankesai8/Online-Book-Store/tree/main/frontend" target="_blank" rel="noopener noreferrer"><button className={classes.btnProj}>BackEnd</button></a>
              
@@ -32,7 +32,7 @@ const MyProjects = () => {
             <p>This is a project built with javascript and dom manipulation to make a Online Music application. A website where you can listen and enjoy songs, go through lyrics and create your own playlists and add your songs.</p>
           </div>
           <div className={classes.btnContainer}>
-            <a href="https://music-player-008.netlify.app/login" target="_blank" rel="noopener noreferrer"><button className={classes.btnProj}>See Preview</button></a>
+            <a href="https://music-player-008.netlify.app/login" target="_blank" rel="noopener noreferrer"><button className={classes.btnProj}>Go Live</button></a>
             <a href="https://github.com/ankesai8/Music-Player-FrontEnd" target="_blank" rel="noopener noreferrer"><button className={classes.btnProj}>FrontEnd</button></a>
             <a href="https://github.com/ankesai8/Music-Player-BackEnd" target="_blank" rel="noopener noreferrer"><button className={classes.btnProj}>BackEnd</button></a>
           </div>
@@ -44,7 +44,7 @@ const MyProjects = () => {
             <p>A Website help us to track the services provides to take care of your car at reasonable costs and afforabdable pricing for everyone .</p>
           </div>
           <div className={classes.btnContainer}>
-            <a href="https://carcare-90.netlify.app/" target="_blank" rel="noopener noreferrer"><button>See Preview</button></a>
+            <a href="https://carcare-90.netlify.app/" target="_blank" rel="noopener noreferrer"><button>Go Live</button></a>
             <a href="https://github.com/ankesai8/Car-Care-FrontEnd" target="_blank" rel="noopener noreferrer"><button>FrontEnd</button></a>
             <a href="https://github.com/ankesai8/Car-Care-BackEnd" target="_blank" rel="noopener noreferrer"><button>BackEnd</button></a>
           </div>
@@ -56,7 +56,7 @@ const MyProjects = () => {
             <p>A Web App which provide recipes to cook various dishes built using React, Node.js</p>
           </div>
           <div className={classes.btnContainer}>
-            <a href="https://mealdb-9989.netlify.app/"  target="_blank" rel="noopener noreferrer"><button>See Preview</button></a>
+            <a href="https://mealdb-9989.netlify.app/"  target="_blank" rel="noopener noreferrer"><button>Go Live</button></a>
             <a href="https://github.com/ankesai8/theMealDB-Recipe-Front-End"  target="_blank" rel="noopener noreferrer"><button>FrontEnd</button></a>
               <a href="https://github.com/ankesai8/theMealDB-Recipe-Back-End"  target="_blank" rel="noopener noreferrer"><button>BackEnd</button></a>  
           </div>
